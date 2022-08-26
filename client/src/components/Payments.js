8840006585e50be43467c15d9a05086126bd36dc
ihyuncho <82439363+ihyuncho@users.noreply.github.com>
2022-08-27 01:13:48 +0900

    fix stripe checkout style

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,10 +5,16 @@ class Payments extends Component {
     render(){
         return (
             <StripeCheckout 
+                name="Emaily"
+                description="$5 for 5 email credits"
                 amount={500} //in cents
-                token={token => console.log(token)} // returned by Stripe
+                token={token => console.log(token)} // returned by Stripe. actually kind of object
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
+           >
+            <button className="btn">
+                Add Credits
+            </button>
+           </StripeCheckout>
         );
     }
 }
