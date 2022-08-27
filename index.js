193cbb1a4c53357c4d7e61bf83ff5372d23c3ada
ihyuncho <82439363+ihyuncho@users.noreply.github.com>
2022-08-27 23:29:47 +0900

    add production mode router to express

--- index.js
+++ index.js
@@ -25,6 +25,20 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production'){
+  // Express will serve up production assets like main.js and main.css
+
+  app.use(express.static('client/build'));// search assets in this folder
+
+  // Express will serve up the inderx.html file
+  //if it doesn't recognize the route
+  // note: this case is the last case when all the condition upper side doesn't match 
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
  
