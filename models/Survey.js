7fa6fad61324b8a36a6eab7dfbade1d85309757d
ihyuncho <82439363+ihyuncho@users.noreply.github.com>
2022-09-12 02:39:49 +0900

    process survey session

--- models/Survey.js
+++ models/Survey.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
     title: String,
     body: String,
     subject: String,
-    recipients: [String], //[ ] = array of string
-    yes: {type: Number, default: 0},
-    no: {type: Number, default: 0}
+    recipients: [RecipientSchema], //[ ] = array of 
+    yes: { type: Number, default: 0 },
+    no: { type: Number, default: 0 },
+    //_## means convention of relationship
+    _user: { type: Schema.Types.ObjectId, ref: 'User' }, // reference for particular user
+    dateSent: Date,
+    lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
